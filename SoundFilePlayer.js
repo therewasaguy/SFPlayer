38d02634f42f4fa912692b8bf9efb01b8e0b1b47
Jason Sigal <jason.sigal@gmail.com>
2015-01-28 12:05:14 -0500

    getPosition returns percent competed

--- SoundFilePlayer.js
+++ SoundFilePlayer.js
@@ -10,25 +10,32 @@
 
 function SoundFilePlayer(ac) {
   var node = ac.createGain();
+  var buffer;
   var bufferSource;
   var bufferSourceProperties = {};
 
+  var counterNode = ac.createBufferSource();
+  var scopeNode = ac.createScriptProcessor( 256, 1, 1);
   var scheduledCallbacks = [];
+  var lastPos;
+  var bufferLength;
 
   ['buffer', 'loop', 'loopStart', 'loopEnd'].forEach( function(name) {
     Object.defineProperty(node, name, makeBufferSourceGetterSetter(name));
   });
 
   node.start = function(when, offset, duration) {
-    var buffer = bufferSourceProperties['buffer'];
+    buffer = bufferSourceProperties['buffer'];
     var when = when || 0;
     initBufferSource();
-    bufferSource.start(when)
+    bufferSource.start(when);
+    counterNode.start(when);
   };
 
   node.stop = function(when) {
     var when = when || 0;
     bufferSource.stop(when);
+    counterNode.stop(when);
   };
 
   node.load = function(src, callback) {
@@ -41,6 +48,7 @@ function SoundFilePlayer(ac) {
     request.onload = function() {
       ac.decodeAudioData(request.response, function(buff) {
         self.buffer = buff;
+        counterNode.buffer = createCounterBuffer( buff );
         if (callback) {
           callback(self);
         }
@@ -54,8 +62,6 @@ function SoundFilePlayer(ac) {
   node.addEvent = function(time, callback) {
     scheduledCallbacks.push({time: callback});
   };
-
-  return node;
 
   function initBufferSource() {
     bufferSource = ac.createBufferSource();
@@ -88,6 +94,34 @@ function SoundFilePlayer(ac) {
       bufferSource[name] = value;
     }
   }
+
+  /*** from http://chinpen.net/talks/wac-paper/#/38 ***/
+  counterNode.connect(scopeNode);
+  scopeNode.connect(ac.destination);
+
+  function createCounterBuffer( audioBuf ) {
+    var array = new Float32Array( node.buffer.length );
+    var audioBuf = ac.createBuffer( 1, node.buffer.length, 44100 );
+    for (var index = 0; index < node.buffer.length; index++ ) {
+      array[ index ] = index;
+    }
+
+    audioBuf.getChannelData( 0 ).set( array );
+    bufferLength = audioBuf.length;
+    return audioBuf;
+  }
+
+  scopeNode.onaudioprocess = function savePosition( processEvent ) {
+    var inputBuffer = processEvent.inputBuffer.getChannelData( 0 );
+    lastPos = inputBuffer[ inputBuffer.length - 1 ] || 0;
+  }
+  // end sonoPort
+
+  node.getPosition = function() {
+    return lastPos / bufferLength;
+  };
+
+  return node;
 
 }
 
--- index.js
+++ index.js
@@ -6,7 +6,6 @@ sf.load('audio/BOPD_-_21205_elevator.mp3', songLoaded);
 
 
 function songLoaded(s) {
-  console.log(s);
   s.connect(ac.destination);
   s.start();
 }
